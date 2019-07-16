771941b0c4404648e23ec8fa4a53003d5f6a86c9
Ancel Miller <ancelclayton@gmail.com>
2019-07-16 16:58:51 -0700

    Auth

--- DatingApp_API/Dtos/UserForRegisterDto.cs
+++ DatingApp_API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp_API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(16, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 16 characters")]
         public string Password { get; set; }
     }
 }
